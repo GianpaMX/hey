9e456d7de815ed37e986fd77f62b98a92656f4e3
Juan Villa <juan64@gmail.com>
2016-06-04 23:19:42 -0500

    first commit

---
+++ app/src/main/java/net/ddns/softux/hey/HeyActivity.java
@@ -0,0 +1,13 @@
+package net.ddns.softux.hey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hey);
+    }
+}
