2dbad284b117977d2d78ffeb8ea334f6a3713c3b
Juan Villa <juan64@gmail.com>
2016-06-12 12:42:19 -0500

    Test modify task adapter

--- app/src/test/java/net/ddns/softux/hey/test/unit/TestTaskListFragment.java
+++ app/src/test/java/net/ddns/softux/hey/test/unit/TestTaskListFragment.java
@@ -13,6 +13,7 @@ import junit.framework.Assert;
 import net.ddns.softux.hey.BuildConfig;
 import net.ddns.softux.hey.HeyApp;
 import net.ddns.softux.hey.R;
+import net.ddns.softux.hey.Task;
 import net.ddns.softux.hey.TaskListFragment;
 import net.ddns.softux.hey.TasksRecylerAdapter;
 import net.ddns.softux.hey.test.HeyDaggerMockRule;
@@ -28,8 +29,10 @@ import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.support.v4.SupportFragmentTestUtil;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by juan on 7/06/16.
@@ -95,6 +98,31 @@ public class TestTaskListFragment {
 
         Assert.assertEquals(stopChildEventListener, startChildEventListener);
     }
+
+    @Test
+    public void testModifyTaskAdapter() {
+        Task task = new Task("Old Title", null);
+
+        DataSnapshot mockSnapshot = mock(DataSnapshot.class);
+        when(mockSnapshot.getValue(any(Class.class))).thenReturn(task);
+        when(mockSnapshot.getKey()).thenReturn("1");
+
+        ArgumentCaptor<ChildEventListener> argumentCaptor = ArgumentCaptor.forClass(ChildEventListener.class);
+        verify(databaseReference).addChildEventListener(argumentCaptor.capture());
+
+        ChildEventListener adapterEventListener = argumentCaptor.getValue();
+        adapterEventListener.onChildAdded(mockSnapshot, null);
+
+        TasksRecylerAdapter adapter = (TasksRecylerAdapter) fragment.getRecyclerView().getAdapter();
+        Assert.assertEquals(task, adapter.getItem(0));
+
+        Task newTask = new Task("New Title", null);
+        when(mockSnapshot.getValue(any(Class.class))).thenReturn(newTask);
+        adapterEventListener.onChildChanged(mockSnapshot, null);
+
+        Assert.assertEquals(newTask, adapter.getItem(0));
+    }
+
 
     public static class TestableTaskListFragment extends TaskListFragment {
         private LinearLayoutManager mockLayoutManager;
