7b82a08d9116c872fc32dd1144242a2b549f3802
Juan Villa <juan64@gmail.com>
2016-06-13 21:33:23 -0500

    Modify Task test

--- app/src/androidTest/java/net/ddns/softux/hey/test/HeyActivityTest.java
+++ app/src/androidTest/java/net/ddns/softux/hey/test/HeyActivityTest.java
@@ -1,11 +1,14 @@
 package net.ddns.softux.hey.test;
 
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import com.firebase.client.Firebase;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 
 import net.ddns.softux.hey.HeyActivity;
@@ -26,10 +29,14 @@ import java.util.Map;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.longClick;
 import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -55,7 +62,7 @@ public class HeyActivityTest {
     DatabaseReference databaseReference;
 
     @Test
-    public void testAdTask() {
+    public void testAddTask() {
         when(firebase.push()).thenReturn(firebase);
         when(firebase.getKey()).thenReturn("1");
 
@@ -73,5 +80,38 @@ public class HeyActivityTest {
         ArgumentCaptor<Map> argumentCaptor = ArgumentCaptor.forClass(Map.class);
         verify(firebase).updateChildren(argumentCaptor.capture());
         Assert.assertEquals(childUpdates, argumentCaptor.getValue());
+    }
+
+    @Test
+    public void taskModifyTask() {
+        activityTestRule.launchActivity(null);
+
+        Task task = new Task("Old Title", null);
+
+        DataSnapshot mockSnapshot = mock(DataSnapshot.class);
+        when(mockSnapshot.getValue(any(Class.class))).thenReturn(task);
+        when(mockSnapshot.getKey()).thenReturn("1");
+
+        ArgumentCaptor<ChildEventListener> argumentCaptor = ArgumentCaptor.forClass(ChildEventListener.class);
+        verify(databaseReference).addChildEventListener(argumentCaptor.capture());
+
+        ChildEventListener adapterEventListener = argumentCaptor.getValue();
+        adapterEventListener.onChildAdded(mockSnapshot, null);
+
+        Task modifiedTask = new Task("Modified Task Title", "Modified description of the task");
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/1", modifiedTask.toMap());
+
+        onView(withId(R.id.task_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, longClick()));
+        onView(withId(R.id.title)).perform(replaceText(modifiedTask.getTitle()), pressImeActionButton());
+        onView(withId(R.id.description)).perform(replaceText(modifiedTask.getDescription()), closeSoftKeyboard());
+        onView(withText(android.R.string.ok)).perform(click());
+
+        ArgumentCaptor<Task> setValueArgumentCaptor = ArgumentCaptor.forClass(Task.class);
+        verify(databaseReference).setValue(setValueArgumentCaptor.capture());
+
+        Assert.assertEquals(modifiedTask.getTitle(), setValueArgumentCaptor.getValue().getTitle());
+        Assert.assertEquals(modifiedTask.getDescription(), setValueArgumentCaptor.getValue().getDescription());
     }
 }
